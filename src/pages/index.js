489fed3921aea901a2b46f03af65e67d3879295f
Mateusz <gorzanski.mateusz@gmail.com>
2019-05-14 23:59:37 +0200

    add header nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'gatsby';
 import './../scss/index.scss';
 
+import GithubIcon from './../images/icons/github.svg';
+
 const IndexPage = () => (
   <>
     <header className="header">
@@ -10,14 +12,15 @@ const IndexPage = () => (
           <Link to="/" className="header__site-logo-permalink">Mateusz Górzański</Link>
         </div>
         <ul className="header__menu">
-          <li className="header__menu-item">Home</li>
-          <li className="header__menu-item">About</li>
-          <li className="header__menu-item">Resume</li>
-          <li className="header__menu-item">Work</li>
-          <li className="header__menu-item">Contact</li>
+          <li className="header__menu-item"><Link className="header__menu-item-link header__menu-item-link--active" to="/">Home</Link></li>
+          <li className="header__menu-item"><a className="header__menu-item-link" href="#about">About</a></li>
+          <li className="header__menu-item"><a className="header__menu-item-link" href="#resume">Resume</a></li>
+          <li className="header__menu-item"><a className="header__menu-item-link" href="#work">Work</a></li>
+          <li className="header__menu-item"><a className="header__menu-item-link" href="#contact">Contact</a></li>
         </ul>
         <ul className="header__links-menu">
-          <li className="header__links-menu-item">GitHub</li>
+          <li className="header__links-menu-item"><Link to="/" className="header__links-menu-item-link">Zmień na polski</Link></li>
+          <li className="header__links-menu-item"><a href="#" className="header__links-menu-item-link">GitHub</a></li>
         </ul>
       </nav>
     </header>
