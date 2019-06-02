b9fbf240525330b6597dcabb2314cfc5830905d3
Mateusz <gorzanski.mateusz@gmail.com>
2019-06-02 14:41:18 +0200

    fix styles, update package.json and add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
-import React from "react"
+import React from 'react';
 
-import SEO from "../components/seo"
+import SEO from '../components/seo';
+import './../scss/404.scss';
 
 const NotFoundPage = () => (
   <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <div id="notfound">
+		<div className="notfound">
+			<div className="notfound-404">
+				<h1>404</h1>
+				<h2>Page not found</h2>
+			</div>
+			<a href="/">Homepage</a>
+		</div>
+	</div>
   </>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
